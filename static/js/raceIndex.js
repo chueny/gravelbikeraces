456a795d621ee49db1319e0a84e66140c1dcc9d7
chueny <chueyangib@gmail.com>
2022-11-09 16:24:50 -0600

    added filter by elevation functionality

--- static/js/raceIndex.js
+++ static/js/raceIndex.js
@@ -4,16 +4,19 @@ searches.forEach(search => search.addEventListener('click', removeHidden));
 function handleLinkClick() {
   document.getElementById("grid").hidden = true;
   document.getElementById("link").hidden = false; 
-  resetSearchHidden();
+  resetHiddenSearch();
+ 
 }
 
 function handleGridClick() {
+  // console.log("inside handlegripclick");
   document.getElementById("grid").hidden = false; 
   document.getElementById("link").hidden = true;
-  resetSearchHidden();  
+  resetHiddenSearch();
+  
 }
 
-function resetSearchHidden(){
+function resetHiddenSearch(){
   //initializes all cards attributes hidden to true;
   raceCards = document.querySelectorAll('.race-card');
   for (const card of raceCards){
@@ -31,7 +34,7 @@ function filterByDistance(value) {
   // console.log(`ValueDistance:${value}`)
   document.getElementById("grid").hidden = true; 
   document.getElementById("link").hidden = true;
-  resetSearchHidden();
+  resetHiddenSearch();
 
   if(value == 25){
     raceCards = document.querySelectorAll('.race-card');
@@ -81,9 +84,72 @@ function filterByDistance(value) {
 }
 
 function filterByElevation(value) {
+  console.log("elevation:", value);
   document.getElementById("grid").hidden = true; 
   document.getElementById("link").hidden = true;
-  resetSearchHidden();
+  resetHiddenSearch();
+
+  if(value == 500){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+      //console.log(elevation);
+      if (elevation > 1 && elevation <=500 ){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } else if(value == 501){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+      //console.log(elevation);
+      if (elevation > 500 && elevation <=1000 ){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } else if(value == 1001){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+      //console.log(elevation);
+      if (elevation > 101 && elevation <=2000 ){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } else if(value == 2001){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+      //console.log(elevation);
+      if (elevation > 2001 && elevation <=3000 ){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } else if(value == 3001){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+     // console.log(elevation);
+      if (elevation > 3001 && elevation <=4000 ){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } else if (value == 4001){
+    raceCards = document.querySelectorAll('.race-card');
+    for (const card of raceCards){
+      elevation = card.dataset.elevation;
+      console.log(elevation);
+      if (elevation > 4001){
+       // console.log(`card:`,card);
+      card.hidden = false;
+      }
+    }
+  } 
 }
 
 function handleFilterButtonClick(type, value) {
